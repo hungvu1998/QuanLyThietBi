eca74c54e76bffaf107a20d6b11dbe8940c572f7
Duong Van Huy <huydv@rikkeisoft.com>
2020-02-18 11:05:21 +0700

    ui login

---
+++ app/src/main/java/com/example/quanlythietbi/ui/loginfragment/EmailViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.quanlythietbi.ui.loginfragment
+
+import androidx.lifecycle.ViewModel
+
+class EmailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/example/quanlythietbi/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/quanlythietbi/ui/splash/SplashActivity.kt
@@ -3,34 +3,41 @@ package com.example.quanlythietbi.ui.splash
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.lifecycle.ViewModelProviders
 import com.example.quanlythietbi.R
 import com.example.quanlythietbi.ui.login.LoginActivity
 import com.example.quanlythietbi.viewmodels.ViewModelProvidersFactory
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.layout_splash.*
 import java.lang.Exception
 import javax.inject.Inject
 
 class SplashActivity : DaggerAppCompatActivity(),SplashNavigator{
 
-
     @Inject
     lateinit var providerFactory: ViewModelProvidersFactory
     lateinit var viewModel: SplashViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_splash)
+        var zoomOutAnim: Animation = AnimationUtils.loadAnimation(applicationContext, R.anim.zoom_out)
+
         viewModel= ViewModelProviders.of(this,providerFactory).get(SplashViewModel::class.java)
         viewModel.setNavigator(this)
-        viewModel.getNavigator()?.runInThread()
+       // viewModel.getNavigator()?.runInThread()
+
 
     }
     override fun runInThread() {
-        Log.d("kiemtra","fffffff")
 
         val thread = object :Thread(){
             override fun run() {
                 try {
+
                     Thread.sleep(5000)
                     val intent = Intent(this@SplashActivity,LoginActivity::class.java)
                     startActivity(intent)
